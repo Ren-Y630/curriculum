ba4eeaf76237461af18e5eba19764736ba9ef9c5
山城廉 <74039009+Ren-Y630@users.noreply.github.com>
2021-06-02 08:26:20 +0900

    1-2

---
+++ JS,jQuery/1-2/main.js
@@ -0,0 +1,22 @@
+// 1-2
+// let name = "hoge";
+// console.log(name);
+// console.log("name");
+
+// name = "fuga";
+// console.log(name);
+
+// let name = "池田";
+// console.log(name + "くん");
+// let number = 10;
+// console.log(number + 3);
+
+// console.log(`${name}くんは${number}月生まれです`);
+
+// 課題
+// const → letに変更
+let price = 100;
+console.log(price * 2);
+
+price = 500;
+console.log(price * 2);
