0304128762e871ede3005a7330473a463543dd2b
山城廉 <74039009+Ren-Y630@users.noreply.github.com>
2021-06-04 08:18:41 +0900

    1-6

---
+++ js 1-6/main.js
@@ -0,0 +1,30 @@
+// let scores = [50, 60, 70, 80, 90];
+// console.log(scores);
+// console.log(scores[3]);
+// console.log(scores.length);
+
+
+// let scores = [50, 60, 70, 80, 90];
+// for (let i = 0; i < 5; i++) {
+//     console.log("得点は" + scores[i] + "点です");
+// }
+
+
+// let scores = [50, 60, 70, 80, 90];
+//scores.length で「5」が取得されるので、i < 5 と同じ意味になります。
+// for (let i = 0; i < scores.length; i++) {
+//     console.log("得点は" + scores[i] + "点です");
+// }
+
+// let human = {name: "yamada", height: 170, weight: 60, gender: "男", age: 30,};
+// console.log(human["name"])
+
+
+// 課題
+let scores = [10, 15, 20, 25];
+console.log(scores);
+
+let car = {gass: 20.5, num: 1234};
+console.log(car);
+console.log(`ガソリンは${car["gass"]}です`);
+console.log(`ナンバーは${car["num"]}です`);
