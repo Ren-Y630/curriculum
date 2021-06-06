79efe30aeb80be1455b2d0bcf095bbdc492742a9
山城廉 <74039009+Ren-Y630@users.noreply.github.com>
2021-06-06 12:26:16 +0900

    2-3

---
+++ js 2-3/main.js
@@ -0,0 +1,13 @@
+ //btnを取得
+const btn = document.getElementById('btn');
+
+ // ボタンがクリックされたら、という処理の始まり
+btn.addEventListener('click', function() {
+     //①配列で「大吉」「中吉」「凶」を準備
+    let omikuji = ["大吉", "中吉", "凶"];
+     //②乱数を発生させて、その値を取得
+    let num = Math.floor(Math.random() * omikuji.length);
+     //③取得した **btn** の「クリック」という文字を配列の中身の文字にランダムに変化させる
+    document.getElementById("btn").innerHTML=(omikuji[num]);
+    // console.log(omikuji[num]);
+}, false);
